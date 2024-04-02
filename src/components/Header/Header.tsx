8438fb51e8a9c2200fbb7531fd7d3739410315a6
LudovicRdt <ludovic.roudot@oclock.school>
2024-04-02 16:47:46 +0200

    feat: creation of Header elements and appearance CSS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,24 @@
+// import du fichier de style Header.scss spécifique au Header
 import './Header.scss';
+// import de l'image du logo à partir du dossier assets
+import logo from '../../assets/logo-gestinvest.svg';
 
 function Header() {
   return (
-    <div>
-      <h1 className="header-title">Header Gestinvest</h1>
-    </div>
+    <header className="menu" id="header">
+      <img className="logo" src={logo} alt="logo Gestinvest" />
+      <nav className="menu-nav">
+        <a className="menu-link" href="#header">
+          Accueil
+        </a>
+        <a className="menu-link" href="#header">
+          Tendances
+        </a>
+      </nav>
+      <button className="menu-btn" type="button">
+        Connexion
+      </button>
+    </header>
   );
 }
 
