c912fcbd6a45cede577b92e66798d63f43a36797
samir-guemmaz <samir.guemmaz@oclock.school>
2024-04-03 01:59:53 +0200

    setting up password matches for registration

--- src/components/ModalLogin/ModalLogin.tsx
+++ src/components/ModalLogin/ModalLogin.tsx
@@ -1,6 +1,4 @@
-// ModalLogin.jsx
-// liste des importations
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
 import { ModalLoginCloseProps } from '../../type/types';
 import './ModalLogin.scss';
 
@@ -8,10 +6,35 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
   // Variable d'état pour gérer l'onglet actif (inscription ou connexion)
   const [activeTab, setActiveTab] = useState('register');
 
+  // États pour stocker les valeurs des champs de mot de passe et de confirmation du mot de passe
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  // État pour suivre si les mots de passe correspondent ou non
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  // Fonction de soumission du formulaire d'inscription
+  const handleRegistreSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      console.log('Les mots de passe correspondent');
+    } else {
+      setPasswordMatch(false);
+    }
+  };
+
+  // Fonction pour vérifier si les mots de passe correspondent à chaque changement dans le champ de confirmation du mot de passe
+  const handleConfirmPasswordChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setConfirmPassword(event.target.value);
+    setPasswordMatch(password === event.target.value); // Vérifie si les mots de passe correspondent
+  };
+
   return (
     <dialog open className="modal">
       <div className="right-element">
-        {/* gestion de la fermeture de la modal */}
+        {/* Gestion de la fermeture de la modal */}
         <button type="button" className="close" onClick={closeModal}>
           X
         </button>
@@ -35,7 +58,7 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
       </div>
       {/* Formulaire d'inscription */}
       {activeTab === 'register' && (
-        <form action="" className="register">
+        <form onSubmit={handleRegistreSubmit} className="register">
           <div className="center-element">
             <label htmlFor="email">E-mail</label>
             <input
@@ -53,6 +76,7 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               name="password"
               placeholder="Veuillez entrer le mot de passe"
               required
+              onChange={(event) => setPassword(event.target.value)} // Met à jour l'état du mot de passe
             />
 
             <label htmlFor="confirm-password">
@@ -62,9 +86,17 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               type="password"
               id="confirm-password"
               name="confirm-password"
+              onChange={handleConfirmPasswordChange} // Appelle la fonction handleConfirmPasswordChange à chaque changement
               placeholder="Veuillez confirmer le mot de passe"
               required
             />
+
+            {/* Affichage du message d'erreur si les mots de passe ne correspondent pas */}
+            {!passwordMatch && (
+              <p className="error-message">
+                Les mots de passe ne correspondent pas.
+              </p>
+            )}
 
             <button className="valid-button" type="submit">
               S&apos;inscrire
@@ -84,7 +116,6 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               placeholder="Veuillez entrer votre adresse e-mail"
               required
             />
-
             <label htmlFor="login-password">Mot de passe</label>
             <input
               type="password"
@@ -93,7 +124,6 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               placeholder="Veuillez entrer le mot de passe"
               required
             />
-
             <button className="valid-button" type="submit">
               Se connecter
             </button>
