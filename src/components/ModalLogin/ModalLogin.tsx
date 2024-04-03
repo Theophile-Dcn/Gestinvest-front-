e421e7029843c3667400f501cca42d6ab7749880
samir-guemmaz <samir.guemmaz@oclock.school>
2024-04-03 13:54:54 +0200

    setting up regex password

--- src/components/ModalLogin/ModalLogin.tsx
+++ src/components/ModalLogin/ModalLogin.tsx
@@ -1,40 +1,73 @@
-import React, { SetStateAction, useState } from 'react';
+import React, { useState } from 'react';
 import { login, register } from '../API/authentification'; // Importez les fonctions d'authentification
-import { ModalLoginCloseProps } from '../../type/types';
 import './ModalLogin.scss';
 
-function ModalLogin({ closeModal }: ModalLoginCloseProps) {
+interface ModalProps {
+  closeModal: () => void;
+}
+
+interface ModalLoginProps extends ModalProps {
+  email: string;
+  password: string;
+  confirmPassword: string; // Cette prop est définie mais non utilisée
+}
+
+function ModalLogin({ email, password, closeModal }: ModalLoginProps) {
   // Variable d'état pour gérer l'onglet actif (inscription ou connexion)
   const [activeTab, setActiveTab] = useState('register');
 
   // États pour stocker les valeurs des champs de mot de passe et de confirmation du mot de passe
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [inputEmail, setInputEmail] = useState(email);
+  const [inputPassword, setInputPassword] = useState(password);
+  const [inputConfirmPassword, setInputConfirmPassword] = useState('');
 
-  // État pour suivre si les mots de passe correspondent ou non
+  // États pour suivre si les mots de passe correspondent ou non
   const [passwordMatch, setPasswordMatch] = useState(true);
+
+  // État pour stocker les messages d'erreur du mot de passe
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState<
+    string | null
+  >(null);
+
+  // Fonction pour valider un mot de passe
+  function validatePassword(password: string) {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return 'Votre mot de passe doit contenir au moins 8 caractères, dont au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.';
+    }
+    return null; // Renvoie null s'il n'y a pas d'erreur
+  }
 
   // Fonction de soumission du formulaire d'inscription
   const handleRegisterSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    if (password === confirmPassword) {
-      try {
-        // Appel à la fonction d'inscription
-        const userData = await register(email, password);
-        console.log('Inscription réussie:', userData);
-        // Ici, vous pouvez effectuer des actions supplémentaires, comme la redirection vers une page de profil.
-      } catch (error) {
-        console.error("Erreur lors de l'inscription:", error);
-        // Gérer les erreurs d'inscription
-      }
-    } else {
-      setPasswordMatch(false);
+    const passwordError = validatePassword(inputPassword);
+    if (inputPassword !== inputConfirmPassword || passwordError) {
+      setPasswordMatch(inputPassword === inputConfirmPassword);
+      setPasswordError(passwordError);
+      setConfirmPasswordError(
+        inputPassword !== inputConfirmPassword
+          ? 'Les mots de passe ne correspondent pas.'
+          : null
+      );
+      return;
+    }
+
+    try {
+      // Appel à la fonction d'inscription
+      const userData = await register(inputEmail, inputPassword);
+      console.log('Inscription réussie:', userData);
+      setActiveTab('login'); // inscription réussie redirection sur la modal inscription
+    } catch (error) {
+      console.error("Erreur lors de l'inscription:", error);
+      // Gérer les erreurs d'inscription
     }
   };
 
   // Fonction de soumission du formulaire de connexion
-  const handleLoginSubmit = async (event: React.FormEvent) => {
+  async function handleLoginSubmit(event: React.FormEvent) {
     event.preventDefault();
     try {
       // Appel à la fonction de connexion
@@ -46,14 +79,25 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
       console.error('Erreur lors de la connexion:', error);
       // Gérer les erreurs de connexion
     }
-  };
+  }
 
   // Fonction pour vérifier si les mots de passe correspondent à chaque changement dans le champ de confirmation du mot de passe
   const handleConfirmPasswordChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setConfirmPassword(event.target.value);
-    setPasswordMatch(password === event.target.value);
+    setInputConfirmPassword(event.target.value);
+    setPasswordMatch(inputPassword === event.target.value);
+    setConfirmPasswordError(
+      inputPassword !== event.target.value
+        ? 'Les mots de passe ne correspondent pas.'
+        : null
+    );
+  };
+
+  // Gérer l'affichage du message d'erreur du mot de passe lorsque l'utilisateur est en train de remplir le champ
+  const handlePasswordInput = (event: React.FormEvent<HTMLInputElement>) => {
+    const passwordError = validatePassword(event.currentTarget.value);
+    setPasswordError(passwordError);
   };
 
   return (
@@ -92,8 +136,8 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               name="email"
               placeholder="Veuillez entrer votre adresse e-mail"
               required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={inputEmail}
+              onChange={(e) => setInputEmail(e.target.value)}
             />
 
             <label htmlFor="password">Mot de passe</label>
@@ -103,9 +147,12 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               name="password"
               placeholder="Veuillez entrer le mot de passe"
               required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} // Met à jour l'état du mot de passe
+              value={inputPassword}
+              onChange={(e) => setInputPassword(e.target.value)}
+              onInput={handlePasswordInput} // Vérification du format du mot de passe à chaque entrée
             />
+            {/* Affichage du message d'erreur pour le mot de passe */}
+            {passwordError && <p className="error-message">{passwordError}</p>}
 
             <label htmlFor="confirm-password">
               Confirmation du mot de passe
@@ -117,14 +164,12 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               onChange={handleConfirmPasswordChange} // Appelle la fonction handleConfirmPasswordChange à chaque changement
               placeholder="Veuillez confirmer le mot de passe"
               required
-              value={confirmPassword}
+              value={inputConfirmPassword}
             />
 
             {/* Affichage du message d'erreur si les mots de passe ne correspondent pas */}
-            {!passwordMatch && (
-              <p className="error-message">
-                Les mots de passe ne correspondent pas.
-              </p>
+            {confirmPasswordError && (
+              <p className="error-message">{confirmPasswordError}</p>
             )}
 
             <button className="valid-button" type="submit">
@@ -145,7 +190,7 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               placeholder="Veuillez entrer votre adresse e-mail"
               required
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setInputEmail(e.target.value)}
             />
             <label htmlFor="login-password">Mot de passe</label>
             <input
@@ -155,7 +200,7 @@ function ModalLogin({ closeModal }: ModalLoginCloseProps) {
               placeholder="Veuillez entrer le mot de passe"
               required
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setInputPassword(e.target.value)}
             />
             <button className="valid-button" type="submit">
               Se connecter
