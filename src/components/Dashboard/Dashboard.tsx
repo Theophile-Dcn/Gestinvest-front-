ee1ffc8e6bd1247efcad6d3f59b2d21539646d4f
LudovicRdt <ludovic.roudot@oclock.school>
2024-04-05 12:35:08 +0200

    feat: creation components Account AssetDetail Dashboard Footer

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Dashboard.scss';
+
+function Dashboard() {
+  return (
+    <div id="dashboard">
+      <h2>Mon tableau de bord</h2>
+      <div>Bilan Dashbord</div>
+      <div>Liste actifs boursiers</div>
+      <div>Liste cryptomonnaies</div>
+    </div>
+  );
+}
+
+export default Dashboard;
