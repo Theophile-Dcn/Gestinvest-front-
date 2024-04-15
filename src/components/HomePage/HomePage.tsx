cd8e51a0f60d35ffc59af6891aa994d4b400ab1a
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-15 09:25:46 +0200

    fix: src img

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import imgHome from '../../assets/img/home.png';
+import imgHome from '../../assets/Gestinvest-dashboard-image.png';
 
 type HomePageProps = {
   isConnected: boolean;
