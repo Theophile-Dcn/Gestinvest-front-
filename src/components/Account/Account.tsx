76ec13d416f6d5fe69e7d2b9e680c71636dcfcb5
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-16 11:22:11 +0200

    feat: implémentation account design & api

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,12 +1,144 @@
-import React from 'react';
-import './Account.scss';
+import React, { useEffect, useState } from 'react';
+import { BaseURL, header } from '../API/API-info';
+import GetAccountInfo from '../API/account-api';
+
+interface UserData {
+  id: number;
+  email: string;
+  last_name: string;
+  first_name: string;
+}
 
 function Account() {
+  const [userData, setUserData] = useState<UserData>({
+    id: 0,
+    email: '',
+    last_name: '',
+    first_name: '',
+  });
+
+  useEffect(() => {
+    const fetchAccountInfo = async () => {
+      try {
+        const data = await GetAccountInfo();
+        setUserData(data.user);
+      } catch (error) {
+        console.error('Error fetching account:', error);
+      }
+    };
+
+    fetchAccountInfo();
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // Envoi des données modifiées à l'API pour mise à jour
+      // Exemple fictif d'appel à l'API de mise à jour
+      await fetch(`${BaseURL}account/`, {
+        method: 'PUT',
+        headers: header,
+        body: JSON.stringify(userData),
+      });
+      console.log('Données du compte mises à jour avec succès');
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour du compte :', error);
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    try {
+      // Appel à l'API pour supprimer le compte
+      await fetch(`${BaseURL}account/${userData.id}`, {
+        method: 'DELETE',
+        headers: header,
+      });
+      console.log('Compte supprimé avec succès');
+      // Vous pouvez également rediriger l'utilisateur vers une page de confirmation ou de déconnexion ici
+    } catch (error) {
+      console.error('Erreur lors de la suppression du compte :', error);
+    }
+  };
+
   return (
-    <div id="account">
-      <h2>Gestion profil</h2>
-      <div>Formulaire</div>
-      <div>Boutons sauvegarder / supprimer le compte</div>
+    <div
+      id="account"
+      className="flex flex-col items-center justify-center w-full p-6"
+    >
+      <h2 className="text-xl uppercase font-bold p-10">Gestion du profil</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full">
+        <div className="flex border border-white rounded-xl bg-white/10 w-full">
+          <div className="flex border-r w-2/5">
+            <label className="p-4" htmlFor="email">
+              Prénom :
+            </label>
+          </div>
+          <input
+            className=" pl-4 bg-transparent"
+            type="text"
+            id="firstName"
+            name="first_name"
+            value={userData.first_name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex border border-white rounded-xl bg-white/10 w-full">
+          <div className="flex border-r w-2/5">
+            <label className="p-4" htmlFor="email">
+              Nom :
+            </label>
+          </div>
+          <input
+            className=" pl-4 bg-transparent"
+            type="text"
+            id="lastName"
+            name="last_name"
+            value={userData.last_name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex border border-white rounded-xl bg-white/10 w-full">
+          <div className="flex border-r w-2/5">
+            <label className="p-4" htmlFor="email">
+              E-mail :
+            </label>
+          </div>
+
+          <input
+            className="pl-4 bg-transparent"
+            type="email"
+            id="email"
+            name="email"
+            placeholder={userData.email}
+            value={userData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex justify-center gap-10">
+          <button
+            className="hover:bg-custom-purple border-buttonColor shadow-lg shadow-indigo-500/30 text-center mt-4 border text-white rounded-full px-2 py-1 lg:m-0 lg:my-8 lg:text-base lg:px-6 lg:py-2"
+            type="submit"
+          >
+            Sauvegarder
+          </button>
+          {/* Bouton pour supprimer le compte */}
+          <button
+            className="hover:bg-custom-purple border-buttonColor shadow-lg shadow-indigo-500/30 text-center mt-4 border text-white rounded-full px-2 py-1 lg:m-0 lg:my-8 lg:text-base lg:px-6 lg:py-2"
+            type="button"
+            onClick={handleDeleteAccount}
+          >
+            Supprimer le compte
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
