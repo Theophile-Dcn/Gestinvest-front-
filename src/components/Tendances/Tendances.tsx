57db166cae5bd0a5aa12f6eeceec9ef5a0c72e3c
Theophile Ducournau <88760829+Theophile-Dcn@users.noreply.github.com>
2024-04-21 23:04:47 +0200

    feat: create tendances page

--- src/components/Tendances/Tendances.tsx
+++ src/components/Tendances/Tendances.tsx
@@ -1,15 +1,12 @@
-import StockWidget from './StockWidget';
-
 const Tendances = () => {
   return (
     <section className="w-[90%] m-auto">
       <h1 className="text-lg text-center py-8">Les tendances des marchés</h1>
-      <div className="">
-        <h2>Top cryptoactifs :</h2>
+      <div className="mb-8">
+        <h2 className="text-lg font-semibold mb-4">Top cryptoactifs :</h2>
       </div>
-      <div className="">
-        <h2>Top stocks :</h2>
-        <StockWidget />
+      <div>
+        <h2 className="text-lg font-semibold mb-4">Top stocks :</h2>
       </div>
     </section>
   );
